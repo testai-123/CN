1284f9925b73e896a62cf6523b4633e5ac747244
testai-123 <151430801+testai-123@users.noreply.github.com>
2023-11-19 23:12:30 +0530

    Update tcp.c

--- tcp.c
+++ tcp.c
@@ -1,78 +1,87 @@
-//server.c
-
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include <unistd.h>
 
 
 int main() {
-    int welcomeSocket, newSocket;
-    char buffer[1024] = "Hello Humans\n";
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addr_size;
 
-    welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+	char server_message[256];
+	printf("Enter message to sent to client: ");
+	scanf("%s",server_message);
 
-    memset(&serverAddr, '\0', sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7891);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+	//create the server socket
+	int server_socket;
+	server_socket = socket(AF_INET,SOCK_STREAM,0);
 
-    bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	// define the server address
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(8000);
+	server_address.sin_addr.s_addr = INADDR_ANY;
 
-    listen(welcomeSocket, 5);
-    printf("Listening...\n");
+	//bins the socket to specified IP and port
+	bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
-    addr_size = sizeof(clientAddr);
-    newSocket = accept(welcomeSocket, (struct sockaddr *) &clientAddr, &addr_size);
+	//listen to the client sockets
+	listen(server_socket,5);
 
-    send(newSocket, buffer, strlen(buffer), 0);
-
-    close(newSocket);
-    close(welcomeSocket);
-    return 0;
+	char buffer[256] = {0};
+	int client_socket;
+	client_socket = accept(server_socket,NULL,NULL);
+	//send the message to client
+	send(client_socket,server_message,sizeof(server_message),0);
+	
+	recv(client_socket,buffer,256,0);
+	printf("The client send the response: ");
+	printf("%s\n",buffer);
+	// close the socket
+	close(server_socket);
+	return 0;
 }
 
-//client.c
+//client
 
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <arpa/inet.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
 #include <unistd.h>
 
 int main() {
-    int clientSocket;
-    char buffer[1024];
-    struct sockaddr_in serverAddr;
-    socklen_t addr_size;
+	// create a socket
+	int network_socket;
+	network_socket = socket(AF_INET,SOCK_STREAM,0);
 
-    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+	// specify an address for the socket
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(8000);
+	server_address.sin_addr.s_addr = INADDR_ANY;
 
-    memset(&serverAddr, '\0', sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7891);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	// check for error while connection
+	if(connection_status == -1)
+	{
+		printf("Theres was an error in making connection!\n\n");
+	}
 
-    addr_size = sizeof(serverAddr);
-    connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+	// recieve data from the server
+	char buffer[256] = {0};
+	recv(network_socket,&buffer,sizeof(buffer),0);
 
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-
-    printf("Data received: %s", buffer);
-
-    close(clientSocket);
-    return 0;
+	printf("The server send the response: %s\n",buffer);
+	
+	char message[256];
+	printf("Send message to server: ");
+	scanf("%s", message);
+	send(network_socket,message,strlen(message),0);
+	
+	close(network_socket);
+	return 0;
 }
-
-
-
-
-
-
-
-
-
