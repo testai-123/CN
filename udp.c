7e4c0a59dc51339d71ccf79a7f270728e9e3f02b
testai-123 <151430801+testai-123@users.noreply.github.com>
2023-11-19 23:33:14 +0530

    Update udp.c

--- udp.c
+++ udp.c
@@ -1,72 +1,108 @@
-//server.c
-
-#include<stdio.h>
 #include<stdlib.h>
+#include<stdio.h>
 #include<string.h>
 #include<sys/socket.h>
+#include<sys/types.h>
 #include<netinet/in.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#include<errno.h>
+#include <unistd.h>
 
-int main(){
-    
-    int sockfd;
-    char buffer[1024] = "Supp\n";
-    struct sockaddr_in serverAddr,client;
-    socklen_t len;
+#define MAX 1024
+#define PORT 8080
 
-    sockfd = socket(AF_INET,SOCK_DGRAM,0);
-
-    memset(&serverAddr,'\0',sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7891);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    bind(sockfd,(struct sockaddr *)(&serverAddr),sizeof(serverAddr));
-
-    memset(&client, 0, sizeof(client));
-    client.sin_family = AF_INET;
-    client.sin_port = htons(7891); // Port of the client
-    client.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP address of the client
-
-    sendto(sockfd,buffer,strlen(buffer),0,(struct sockaddr *)(&client),sizeof(client));
-
-    close(sockfd);
-
-    return 0;
+int main() {
+	int server_socket;
+	char buffer[MAX];
+	char hello[256];
+	
+	// creation of server socket
+	server_socket = socket(AF_INET,SOCK_DGRAM,0);
+	if(server_socket < 0)
+	{
+		perror("Error in creating socket!");
+		exit(EXIT_FAILURE);
+	}
+	
+	//set memory for the client socket address
+	struct sockaddr_in client_adddress;
+	memset(&client_adddress,0,sizeof(client_adddress));
+	
+	// specify address of server socket
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	
+	// bind the server socket with the available client socket
+	bind(server_socket,(const struct sockaddr *)&server_address,sizeof(server_address));
+	
+	int n;
+	socklen_t len;
+	len = sizeof(client_adddress);
+	
+	// recvfrom recieves the hello message from socket
+	n = recvfrom(server_socket,(char *)buffer,MAX,MSG_WAITALL,(struct sockaddr *)&client_adddress,&len);
+	buffer[n] = '\0';
+	printf("Client: %s\n",buffer);
+	printf("\nEnter message: ");
+	scanf("%s", hello);
+	sendto(server_socket,(char *)hello,strlen(hello),MSG_CONFIRM,(const struct sockaddr *)&client_adddress,sizeof(client_adddress));
+	printf("\nMessage Sent to client\n");
+	close(server_socket);
+	return 0;
 }
 
-//client.c
+//client
 
-#include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+#include<stdio.h>
 #include<sys/socket.h>
+#include<sys/types.h>
 #include<netinet/in.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#include<errno.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(){
 
-    int clientSocket;
-    char buffer[1024];
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t len;
-
-    clientSocket = socket(AF_INET,SOCK_DGRAM,0);
-
-    memset(&serverAddr,'\0',sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7891);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    bind(clientSocket,(struct sockaddr *) &serverAddr,sizeof(serverAddr));
-
-    len = sizeof(clientAddr);
-
-    recvfrom(clientSocket,buffer,1024,0,(struct sockaddr *)&clientAddr,&len);
-    printf("Data Received : %s",buffer);
-
-    close(clientSocket);
-}
-
+#define MAX 1024
+#define PORT 8080
+int main() {
+	
+	int network_socket;
+	char buffer[MAX];
+	char hello[256];
+	
+	// creation of client socket
+	network_socket = socket(AF_INET,SOCK_DGRAM,0);
+	if(network_socket < 0)
+	{
+		perror("Socket creation failed!");
+		exit(EXIT_FAILURE);
+	}
+	
+	// define server address
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	
+	int n;
+	socklen_t len;
+	printf("\nEnter message: ");
+	scanf("%s", hello);
+	// sendto function in UDP sends the hello message to server
+	sendto(network_socket,(char *)hello,strlen(hello),MSG_CONFIRM,(const struct sockaddr *)&server_address, sizeof(server_address));
+	
+	printf("Message sent to server!");
+	
+	len = sizeof(server_address);
+	
+	// recvfrom recieves the message from server
+	n = recvfrom(network_socket,buffer,MAX,MSG_WAITALL,(struct sockaddr *)&server_address, &len);
+	
+	buffer[n] = '\0';
+	printf("\nServer: %s\n",buffer);
+	
+	close(network_socket);
+	return 0;
+	}
